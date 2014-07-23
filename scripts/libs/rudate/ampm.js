a5fd2413bc42ca1b87071969c8e64b78be558185
Dmitry Shvalyov <dsh-creative+subscription@yandex.ru>
2014-07-23 22:05:06 +0400

    js линтинг

--- scripts/libs/rudate/ampm.js
+++ scripts/libs/rudate/ampm.js
@@ -3,25 +3,18 @@
 define(function() {
 	'use strict';
 
-	var ampm = [
-		'дня|pm',
-		'вечера|pm',
-		'ночи|am',
-		'утра|am'
-	];
+	var ampm = ['дня|pm', 'вечера|pm', 'ночи|am', 'утра|am'],
+	    expression = new RegExp(ampm.map(function(item) {
+			return '(' + item + ')';
+		}).join('|'), 'g');
 
-	var expression = new RegExp(ampm.map(function(item) {
-		return '(' + item + ')';
-	}).join('|'), 'g');
-
-	return function(body, date) {
-		var matches = body.match(expression),
-		    hours = date.hours();
+	return function(body) {
+		var matches = body.match(expression);
 
 		if (matches) {
 			return ampm.map(function(item){
 				return 0 === item.split('|')[0].indexOf(matches) ? item.split('|')[1] : null;
 			}).join('');
 		}
-	}
+	};
 });
